ad2753bbf9c6420bfd649a7115c8457dc6e864a8
wochap <w4ch4pgmail.com>
2015-04-02 18:18:02 -0500

    agregando estilos

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(function () {
 	var PATH = "http://localhost:3000";
 
 	// variables Formulario
+	var $result = $('#result');
 	var $videoInput = $('#youtube-url');
 	var $button = $('#youtube-button');
 
@@ -29,6 +30,27 @@ $(function () {
 
 	function fillVideoInfo (jsonData) {
 		var videoTitle = jsonData.video.title;
+		var v360p = jsonData.video.links.mp4.v360p || null;
+		var v480p = jsonData.video.links.mp4.v480p || null;
+		var v720p = jsonData.video.links.mp4.v720p || null;
+		
+		var html = '';		
+		html += '<div class="video"><iframe width="854" height="510" src="https://www.youtube.com/embed/'+jsonData.video.id+'" frameborder="0" allowfullscreen></iframe></div>';
+		html += '<ul class="Links">';
+		if (v360p !=null)
+			html += '<li class="Link-option"><a href="'+v360p+'" target="_blanc" class="link btn-succes btn-lg">360p</a></li>'
+		if (v480p !=null)
+			html += '<li class="Link-option"><a href="'+v480p+'" target="_blanc" class="link btn-succes btn-lg">480p</a></li>'
+		if (v720p !=null)
+			html += '<li class="Link-option"><a href="'+v720p+'" target="_blanc" class="link btn-succes btn-lg">720p</a></li>'
+		html += '</ul>';
+		
+			 
+		
+		
+
+		$result.html(null);
+		$result.append(html);
 
 		$videoTitle.html(videoTitle);
 		NProgress.done();
@@ -37,5 +59,3 @@ $(function () {
 	$button.on('click', onSubmit);
 });
 
-// Access-Control-Allow-Origin: *
-// CORS
