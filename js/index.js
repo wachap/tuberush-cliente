2d193a1fe54cfcbe3e4fcbc63dfe92785496831c
Gean Carlos <w4ch4p@gmail.com>
2015-04-05 18:11:39 -0500

    manejando errores

--- js/index.js
+++ js/index.js
@@ -1,61 +1,88 @@
 $(function () {
-	// GLOBAL
-	var PATH = "http://localhost:3000";
 
-	// variables Formulario
-	var $result = $('#result');
-	var $videoInput = $('#youtube-url');
-	var $button = $('#youtube-button');
 
-	// variables DOM
-	var $videoTitle = $('.MainSection-title');
+// Globals
 
-	// AJAX Functions
-	function getVideo (url, callback) {
-		$.ajax({
-			data: {
-				url: url
-			},
-			url: PATH+"/urls/"
-		})
-		.done( callback );
+var PATH = "http://localhost:3000";
+
+
+// AJAX Functions
+
+function getVideo (url, callback) {
+	$.ajax({
+		data: {
+			url: url
+		},
+		url: PATH+"/urls/",
+		error: onError
+	})
+	.done( callback );
+};
+
+
+// Template Functions
+
+function videoTemplate (video) {
+	var html = '',
+		v360p = video.links.mp4.v360p,
+		v480p = video.links.mp4.v480p,
+		v720p = video.links.mp4.v720p;
+
+	html += '<div class="video"><iframe width="854" height="510" src="https://www.youtube.com/embed/'+video.id+'" frameborder="0" allowfullscreen></iframe></div>';
+	html += '<ul class="Links">';
+	if (v360p !=null)
+		html += '<li class="Link-option"><a href="'+v360p+'" download class="link btn-succes btn-lg">360p</a></li>'
+	if (v480p !=null)
+		html += '<li class="Link-option"><a href="'+v480p+'" download class="link btn-succes btn-lg">480p</a></li>'
+	if (v720p !=null)
+		html += '<li class="Link-option"><a href="'+v720p+'" download class="link btn-succes btn-lg">720p</a></li>'
+	html += '</ul>';
+
+	return html
+};
+
+
+var $videoInput = $('#youtube-url');
+var $button = $('#youtube-button');
+var $resultOut = $('#result');
+
+$videoInput.on('keyup', onKeyUp);
+$button.on('click', onSubmit);
+
+var $videoTitle = $('.MainSection-title');
+
+function onKeyUp (evt) {
+	if (evt.keyCode == 13) {
+		onSubmit();
+	};
+};
+
+function onSubmit () {
+	getVideo($videoInput.val(), fillVideoInfo);
+	NProgress.start();
+	$resultOut.html( '<p class="loading">cargando...</p>' );
+};
+
+function onError() {
+	NProgress.done();
+	$resultOut.html( '<p class="error">Error... :(</p>' );
+}
+
+// Fill Functions
+
+function fillVideoInfo (jsonData) {
+	if (jsonData.video.success == false || jsonData.error || !jsonData) {
+		return onError();
 	};
 
+	var videoTitle = jsonData.video.title;
+	$videoTitle.html(videoTitle);
 
-	function onSubmit () {
-		getVideo($videoInput.val(), fillVideoInfo);
-		NProgress.start();
-		$videoTitle.html('Cargando...');
-	};
+	var html = videoTemplate(jsonData.video);
+	$resultOut.html(html);
+	NProgress.done();
+};
 
-	function fillVideoInfo (jsonData) {
-		var videoTitle = jsonData.video.title;
-		var v360p = jsonData.video.links.mp4.v360p || null;
-		var v480p = jsonData.video.links.mp4.v480p || null;
-		var v720p = jsonData.video.links.mp4.v720p || null;
-		
-		var html = '';		
-		html += '<div class="video"><iframe width="854" height="510" src="https://www.youtube.com/embed/'+jsonData.video.id+'" frameborder="0" allowfullscreen></iframe></div>';
-		html += '<ul class="Links">';
-		if (v360p !=null)
-			html += '<li class="Link-option"><a href="'+v360p+'" target="_blanc" class="link btn-succes btn-lg">360p</a></li>'
-		if (v480p !=null)
-			html += '<li class="Link-option"><a href="'+v480p+'" target="_blanc" class="link btn-succes btn-lg">480p</a></li>'
-		if (v720p !=null)
-			html += '<li class="Link-option"><a href="'+v720p+'" target="_blanc" class="link btn-succes btn-lg">720p</a></li>'
-		html += '</ul>';
-		
-			 
-		
-		
 
-		$result.html(null);
-		$result.append(html);
-
-		$videoTitle.html(videoTitle);
-		NProgress.done();
-	};
-
-	$button.on('click', onSubmit);
 });
 
